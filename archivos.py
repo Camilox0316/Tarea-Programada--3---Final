674552fe1d8d91e233f44f88be98c60db180b296
Camilox0316 <camsanchezr03@gmail.com>
2021-11-10 04:13:28 -0600

    Creacion de BD códigos

--- archivos.py
+++ archivos.py
@@ -15,3 +15,30 @@ Versión:              Python 3.9.6
 ##############################################################
 #####              Definición de Funciones               #####
 ##############################################################
+def guardarTXT(filePath, stringToWrite):
+    """
+    Función: Guarda un archivo con extensión .txt
+    Entradas:
+    -filePatch(str): Es el nombre del archivo 
+    -stringToWrite(str): Es la informacion a aguardar en el archivo
+    Salidas: N/A
+    """
+    fo = open(filePath, "w")   #crea el file si no existe
+    fo.write (str(stringToWrite))
+    fo.close()
+    return ""
+
+def leerTXT(filePath):
+    """
+    Función: Carga la información que haya en un archivo txt
+    Entradas:
+    -filePath(str): Es el nombre del archivo
+    Salidas: Lo que se encuentre en el archivo
+    """
+    try:
+        fo = open (filePath, 'r',  encoding="utf8")
+        resultado = fo.read()
+        fo.close()
+        return resultado
+    except:
+        return ""
--- funciones.py
+++ funciones.py
@@ -10,8 +10,47 @@ Versión:              Python 3.9.6
 #####              Importación de Librerías              #####
 ##############################################################
 
-
+from archivos import *
 
 ##############################################################
 #####              Definición de Funciones               #####
 ##############################################################
+def pasarDiccionario():
+    parchivo = leerTXT("BDPostalCR.txt").splitlines()
+    diccionario = {}
+    for elemento in parchivo:
+        actual = elemento.split(";") # [provincia, canton, distrito, codigo]
+        provincia, canton = actual[0], actual[1]
+        if provincia not in diccionario.keys():
+            diccionario[provincia] = {}
+        if canton not in diccionario[provincia].keys():
+            diccionario[provincia].update({canton: {}})  
+        diccionario[provincia][canton].update({actual[-2]: actual[-1]})
+    return diccionario
+
+
+def conseguirProvincias(pdict):
+    return pdict.keys()
+def conseguirCantones(pdcit, provincia):
+    return pdcit[provincia].keys()
+def conseguirDistritos(pdict, pcanton, pprovincia):
+    return pdict[pprovincia][pcanton].keys()
+
+def conseguirCodigo(pdict, pprovincia, pcanton, pdistrito):
+    return pdict[pprovincia][pcanton][pdistrito]
+
+def crearCodigosPostales():
+    diccionario = pasarDiccionario()
+    matriz = []
+    for indice, llave in enumerate(diccionario.keys()):
+        matriz.append([llave]) # [[san jose, []]]
+        for canton, distrito in diccionario[llave].items():
+            matriz[indice].append([canton, distrito]) 
+    return matriz
+
+
+BD = pasarDiccionario()
+"""
+Se va a pedir el codigo de San José -> Santa Ana -> Piedades; el cual es: 10905
+"""
+print(f'Y el código postal es: {conseguirCodigo(BD, "San José", "Santa Ana", "Piedades")}')
