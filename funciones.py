af788cb344a788bb5adcf4b9c6567be7992916bf
marioBarbozaA <marionetabar1@gmail.com>
2021-11-16 23:28:57 -0600

    cambio

--- funciones.py
+++ funciones.py
@@ -10,8 +10,12 @@ Versión:              Python 3.9.6
 #####              Importación de Librerías              #####
 ##############################################################
 
+from datetime import *
+from pickle import *
 from archivos import *
 import re
+from fpdf import FPDF
+import smtplib
 
 ##############################################################
 #####              Definición de Funciones               #####
@@ -173,3 +177,48 @@ def validarCodigoPostal(codigo):
     lista = []
     return codigo in lista
     return matriz
+def creaPdf(nombre,especifica,general,codigo):
+    """
+    Funcionalidad: Crea un Pdf de la etiqiueta 
+    Entradas: Na
+    Salidas: Reporte pdf  
+    """
+    class PDF(FPDF):
+        def header(self):
+            for elemento in (200,0,100):
+                self.image('correo.png',elemento,-25,80)
+            self.ln(60)
+    pdf = PDF('L','mm','Letter')
+    # Agregar Página
+    pdf.add_page()
+    pdf.add_font(family = 'serif', style = 'B', fname = 'fuentes/serif.ttf' , uni=True)
+    pdf.add_font(family = 'serif2', style = '', fname = 'fuentes/serif2.ttf' , uni=True)
+    pdf.set_font('serif','B',12)
+    pdf.cell(220,10,nombre,ln=True,align='l',border='T,R,L')
+    pdf.set_font('serif2','',12)
+    pdf.cell(220,10,especifica,align='l',fill=0,border='R,L',ln=1)
+    pdf.cell(220,10,general,align='l',fill=0,border='R,L',ln=2)
+    pdf.cell(220,10,codigo,align='l',fill=0,border='R,L',ln=3)
+    pdf.cell(220,10,'COSTA RICA',align='l',fill=0,border='R,L,B',ln=4)
+    pdf.image('stamp.png',180,68,50,50)
+    for elem in (200,100,0):
+        pdf.image('correo.png',elem,150,80)
+    pdf.output((f'{nombre}.pdf'))
+    return 'Archivo Creado'
+#creaPdf('Mario Barboza Artavia','Alajuela, San Ramon, San Ramón ','AV: 2 C: 2 #55 ' ,'1101010')
+
+def enviarCorreo(correo):
+    """
+    Funcionalidad: Envia correo  
+    Entradas: correo
+    Salidas: NA  
+    """
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login('correoscrmariocamilo@gmail.com','tareaProgramada')
+    server.sendmail('correoscrmariocamilo@gmail.com',correo,'La proxima fecha de entrega sera el dia (poner el dia no lo entendi)')
+    server.quit()
+    return 'Correo enviado'
+#enviarCorreo('marionetabar1@gmail.com')
+print(str(date.today() + datetime.timedelta(days=1)))
+
